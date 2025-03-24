d9152daee7ab7243fb8246618ad4afc6d273237b
Mihir Mane <mihirmane3@gmail.com>
2025-03-24 18:03:30 -0500

    Update assign2.tf

--- assign2.tf
+++ assign2.tf
@@ -53,7 +53,7 @@ resource "google_compute_router_nat" "nat_config" {
   router                             = google_compute_router.nat_router.name
   region                             = google_compute_router.nat_router.region
   nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_IP_RANGES"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
 
 # Create Compute Engine Instance with Flask App
