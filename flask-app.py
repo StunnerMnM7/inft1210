8b7168b978f65ef91d47dde01a738932e0370075
Mihir Mane <mihirmane3@gmail.com>
2025-04-05 13:30:01 -0500

    Update flask-app.py

--- flask-app.py
+++ flask-app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello Cloud from Mihir Limbad and this is updated Hello Cloud' 
+    return 'Hello from Mihir ECS Container ' 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
