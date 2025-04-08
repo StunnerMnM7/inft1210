59ea2a2c451502b0f08facfa4b9d0bcdd9ab3be9
Mihir Mane <mihirmane3@gmail.com>
2025-04-08 11:58:03 -0500

    uodate

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello from Mihir ECS Container ' 
+    return 'Hello from Mihir ECS Container' 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
